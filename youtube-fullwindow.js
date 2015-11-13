4150e87bb39b82c073a3cbafb7c4ac1467b49f97
Lasercar7 <lasercar@users.noreply.github.com>
2015-11-12 23:37:31 -0600

    Update youtube-fullwindow.js

--- youtube-fullwindow.js
+++ youtube-fullwindow.js
@@ -1,4 +1,6 @@
 (function() {
   var url = location.href;
   var videoCode = url.match(/watch\?v=([a-z0-9]+)/i)[1];
+  var newUrl = "https://youtube.com/v/" + videoCode;
+  window.location = newUrl;
 })();
