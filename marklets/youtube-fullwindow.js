900ca6910bfa06df09cc34ba9dd9e03153fa2657
Lasercar7 <lasercar@users.noreply.github.com>
2015-11-13 19:08:32 -0600

    fix video id regex

--- marklets/youtube-fullwindow.js
+++ marklets/youtube-fullwindow.js
@@ -1,6 +1,6 @@
 (function() {
   var url = location.href;
-  var videoCode = url.match(/watch\?v=([a-z0-9]+)/i)[1];
+  var videoCode = url.match(/\?v=([a-zA-Z0-9_\-]+)/)[1];
   var newUrl = "https://youtube.com/v/" + videoCode;
   window.location = newUrl;
 })();
